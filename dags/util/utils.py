cda451984b5dac0d84bb12283c6c34e0641dab7f
Prasad Hadbe <Prasadahadbe313@gmail.com>
2025-04-24 05:31:47 +0530

    Trigger redeploy

--- dags/util/utils.py
+++ dags/util/utils.py
@@ -1,13 +1,15 @@
 from datetime import datetime, timedelta
 
-def get_daily_chunks(start: str, end: str):
+def get_weekly_chunks(start: str, end: str):
     chunks = []
     current = datetime.strptime(start, "%Y-%m-%d")
     end_dt = datetime.strptime(end, "%Y-%m-%d")
 
-    while current <= end_dt:
-        day = current.strftime("%Y-%m-%d")
-        chunks.append({"start_date": day, "end_date": day})
-        current += timedelta(days=1)
+    while current < end_dt:
+        next_dt = min(current + timedelta(days=7), end_dt) #backfilling from 2024-01-01 by months 
+        chunks.append({
+            "start_date": current.strftime("%Y-%m-%d"),
+            "end_date": next_dt.strftime("%Y-%m-%d")
+        })
+        current = next_dt
     return chunks
-
